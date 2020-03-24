0eee4e436cf2e600331e3db38b6b60d807f6234d
Matheus Alves <matheusmotta2005@gmail.com>
2020-03-24 15:17:25 +0000

    Added search feature

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import sort
+from search import Search
 from config import *
 
 def reset(text):
@@ -6,24 +7,37 @@ def reset(text):
     input = INPUT_ARRAY[:]
     print("===== "+str(text)+" =====")
 
+def test_sort(enabled):
+    if enabled:   
+        if SORT_BUBBLE_ENABLED:
+            # Bubble algorithm
+            reset("Bubble Sort")
+            sort.bubble(input)
+
+        if SORT_INSERT_ENABLED:
+            # Insert algorithm
+            reset("Insertion Sort")
+            sort.insert(input)
+
+        if SORT_MERGE_ENABLED:
+            # Merge algorithm
+            reset("Merge Sort")
+            sort.merge(input)
+
+        if SORT_QUICK_ENABLED:
+            # QuickSOrt algorithm
+            reset("Quick Sort")
+            sort.quick(input)
+            
+def test_search():
+    input = INPUT_ARRAY[:]
+    sort.merge(input)
+
+    search = Search(input)
+    search.binarySearch(7)
+
 def main():
-    if SORT_BUBBLE_ENABLED:
-        # Bubble algorithm
-        reset("Bubble Sort")
-        sort.bubble(input)
+    test_sort(False)
+    test_search()    
 
-    if SORT_INSERT_ENABLED:
-        # Insert algorithm
-        reset("Insertion Sort")
-        sort.insert(input)
-
-    if SORT_MERGE_ENABLED:
-        # Merge algorithm
-        reset("Merge Sort")
-        sort.merge(input)
-
-    if SORT_QUICK_ENABLED:
-        # QuickSOrt algorithm
-        reset("Quick Sort")
-        sort.quick(input)
 main()
